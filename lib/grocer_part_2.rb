b592f90b1240ed23b165a8355fbb2aaa32b17cd1
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 21:58:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,1 +1,15 @@
+def apply_coupons(cart, coupons)
+  # code here	coupons.each do |coupon|
+  name = coupon[:item] #makes things easier
 
+  if cart[name] && cart[name][:count] >= coupon[:num]
+    if cart["#{name} W/COUPON"]
+      cart["#{name} W/COUPON"][:count] += coupon[:num]
+    else
+      cart["#{name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[name][:clearance], :count => coupon[:num]}
+    end
+  cart[name][:count] -= coupon[:num]
+  end
+end
+cart
+end
