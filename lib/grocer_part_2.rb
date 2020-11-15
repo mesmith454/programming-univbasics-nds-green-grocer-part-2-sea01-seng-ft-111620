45af5279622745b206ffb35b5c4ed36705f699c6
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 22:03:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,29 +1,9 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-coupCart = cart
-  count = 0
-  while coupons[count] do
-    if cart[coupons[count][:item]]
-      if cart[coupons[count][:item]][:count] >= coupons[count][:num]
-        coupCart[coupons[count][:item]][:count] = coupCart[coupons[count][:item]][:count] - coupons[count][:num]
-        coupItem = "#{coupons[count][:item]} W/COUPON"
-        coupPrice = coupons[count][:cost] / coupons[count][:num]
-        if coupCart[coupItem]
-          coupCount = coupCart[coupItem][:count] + coupons[count][:num]
-        else
-          coupCount = coupons[count][:num]
-        end
-        coupCart[coupItem] = {:price => coupPrice, :clearance => coupCart[coupons[count][:item]][:clearance], :count => coupCount}
-        count += 1
-      else
-        count += 1
-      end
-    else
-      count += 1
-    end
-  end
-  coupCart
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
 end
 
 def apply_clearance(cart)
