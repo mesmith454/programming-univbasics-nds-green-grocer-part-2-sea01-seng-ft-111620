b660fed6279af359d53f6041706caec7c87a45b6
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 22:05:44 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,19 +7,44 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  # code here
+  discount = 0.20
+  cart.each do |item, details|
+    if cart[item][:clearance] == true
+      cart[item][:price] = (cart[item][:price]*discount).round(1)
+    end
+  end
+  cart
 end
 
-def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+def checkout(cart: [], coupons: [])
+  # code here
+  total = 0
+  cart = consolidate_cart(cart)
+  
+  if cart.length == 1
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    if cart_clearance.length > 1
+      cart_clearance.each do |item, details|
+        if details[:count] >=1
+          total += (details[:price]*details[:count])
+        end
+      end
+    else
+      cart_clearance.each do |item, details|
+        total += (details[:price]*details[:count])
+      end
+    end
+  else
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    cart_clearance.each do |item, details|
+      total += (details[:price]*details[:count])
+    end
+  end
+  if total > 100
+    total = total*(0.90)
+  end
+  total
 end
