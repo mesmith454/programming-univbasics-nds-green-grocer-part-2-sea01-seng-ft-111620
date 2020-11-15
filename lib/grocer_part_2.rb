c9948fccbab46f7e487b979a1cb2c27a74291475
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 22:00:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,44 +1,25 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  coupons.each do |coupon|
-  name = coupon[:item] #makes things easier
-
-  if cart[name] && cart[name][:count] >= coupon[:num]
-    if cart["#{name} W/COUPON"]
-      cart["#{name} W/COUPON"][:count] += coupon[:num]
-    else
-      cart["#{name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[name][:clearance], :count => coupon[:num]}
-    end
-  cart[name][:count] -= coupon[:num]
-  end
-end
-cart
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
 end
 
 def apply_clearance(cart)
-   cart.each do |item, info|
-    if info[:clearance] == true 
-      info[:price] = (info[:price] * 0.8).round(2)
-    end
-  end
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
-   total = 0 
-  new_cart = consolidate_cart(cart)
-  coupon_cart = apply_coupons(new_cart, coupons)
-  clearance_cart = apply_clearance(coupon_cart)
-
-  clearance_cart.each do |item, info|
-    total += (info[:price] * info[:count])
-  end
-
-#additional discount if over 100
-if total > 100
-  total = (total * 0.9).round(2)
-else
-  total
-end
-
+  # Consult README for inputs and outputs
+  #
+  # This method should call
+  # * consolidate_cart
+  # * apply_coupons
+  # * apply_clearance
+  #
+  # BEFORE it begins the work of calculating the total (or else you might have
+  # some irritated customers
 end
