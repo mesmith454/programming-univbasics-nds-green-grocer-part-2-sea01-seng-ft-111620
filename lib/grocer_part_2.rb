b168703e75900dbda4874d3d12c9f10a8e5b4520
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 21:49:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,20 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
   coupons.each do |coupon| 
     coupon.each do |key, value| 
       name = coupon[:item] 
-
-      if cart[name] && cart[name][:count] >= coupon[:num] 
+      if cart[name] && cart[name][:count] >= coupon[:num]
         if cart["#{name} W/COUPON"] 
           cart["#{name} W/COUPON"][:count] += 1 
         else 
           cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
           :clearance => cart[name][:clearance], :count => 1} 
         end 
-
       cart[name][:count] -= coupon[:num] 
+      end 
     end 
   end 
-end 
   cart 
 end
 
@@ -27,9 +22,9 @@ def apply_clearance(cart)
     cart.each do |item, attribute| 
     if attribute[:clearance] == true 
       attribute[:price] = (attribute[:price] *
-      0.8).round(2) 
+      (0.8).round(2) 
     end 
-  end 
+    end 
 cart 
 end
 
