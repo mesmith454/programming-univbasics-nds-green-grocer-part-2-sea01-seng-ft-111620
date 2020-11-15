8744dbee64103ec1a3216647ff10c61e3f257f1c
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-15 22:00:34 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,24 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  for i in 0...coupons.length do
+    name = coupons[i][:item]
+    if (cart.keys.include?(name)) && (cart[name][:count] >= coupons[i][:num])
+      cart[name][:count] -= coupons[i][:num]
+      if cart.keys.include?("#{name} W/COUPON")
+        cart["#{name} W/COUPON"][:count] += coupons[i][:num]
+      else
+        cart["#{name} W/COUPON"] = {}
+        cart["#{name} W/COUPON"][:count] = coupons[i][:num]
+        cart["#{name} W/COUPON"][:price] = coupons[i][:cost]/coupons[i][:num]
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      # if cart[name][:count] == 0
+      #   cart.delete(name)
+      # end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
